cd77dc7ccb1840cb0e4c7f1dfdd22ef2dbae1bad
OlukaGibson <olukagibson7@gmail.com>
2024-11-08 14:57:43 +0300

    Apis done and tested

--- tobin.py
+++ tobin.py
@@ -8,7 +8,7 @@ def hex_to_bin(hex_file_path, bin_file_path):
     ih.tobinfile(bin_file_path)
     print(f"Converted {hex_file_path} to {bin_file_path}.")
 
-hex_file_path = "AirQo-Grev6B-V42_only_6B3.ino.mega.hex" 
+hex_file_path = "AirQo-Grev6B-V42_only_6B7.1_bulkChanges.ino.mega.hex" 
 bin_file_path = "output.bin" 
 
 hex_to_bin(hex_file_path, bin_file_path)
